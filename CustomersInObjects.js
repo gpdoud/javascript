6593be906937770e12f59c5357cccc42fb4df02e
Greg <gpdoud@gmail.com>
2017-05-19 11:04:14 -0400

    inial commit - CustomersInObjects

---
+++ CustomersInObjects.js
@@ -0,0 +1,26 @@
+var customers = [
+	{ 
+		id: 1000, 
+		name: "Waldorf Electonics", 
+		address: "123 Any Street",
+		city: "Berlin",
+		state: "Germany",
+		postalcode: "ABC123" 
+	}, 
+	{
+		id: 1010, 
+		name: "Google", 
+		address: "1 Google Way",
+		city: "Mountain View",
+		state: "California",
+		postalcode: "12345" 
+	}, 
+	{
+		id: 1020, 
+		name: "Native Instruments", 
+		address: "2 Guitar Way",
+		city: "New York",
+		state: "New York",
+		postalcode: "10101" 
+	}
+]
